4c36048991872ead128ebff2511cc35bce311b93
Urban <urbainishimwe@gmail.com>
2020-02-08 22:31:50 +0200

    config super admin

--- src/mongo/migrations/create.js
+++ src/mongo/migrations/create.js
@@ -2,11 +2,16 @@ import connection, { connected, error, disconnected, termination } from '..';
 import { User } from '../models';
 import Util from '../../utils';
 
-const { SUPER_PASSWORD, password = Util.hashPassword(SUPER_PASSWORD) } = process.env;
+const {
+  SUPER_PASSWORD,
+  SUPER_USERNAME,
+  SUPER_EMAIL,
+  password = Util.hashPassword(SUPER_PASSWORD),
+} = process.env;
 connection.on('connected', async () => {
   console.log(connected(`seeding tables at database ${connection.db.databaseName}!`));
   try {
-    await User.create({ email: 'mcfrank@gmail.com', username: 'mcfrank', role: 'superadmin', password, name: 'Frank Mutabazi' });
+    await User.create({ email: SUPER_EMAIL, username: SUPER_USERNAME, role: 'superadmin', password, name: 'Administrator' });
     console.log(termination('Tables seeded successfully!'));
   } catch (err) {
     console.log(error('Seeds error', err.message));
